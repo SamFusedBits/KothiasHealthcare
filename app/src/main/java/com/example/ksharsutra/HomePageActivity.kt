dcd1666b26de3d629aa4575f2ee05fd22316978a
Sahil Memon <sahil.infosphere@gmail.com>
2024-05-09 07:10:57 +0530

    Initial Commit

--- app/src/main/java/com/example/ksharsutra/HomePageActivity.kt
+++ app/src/main/java/com/example/ksharsutra/HomePageActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ksharsutra
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,7 +10,12 @@ class HomePageActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
-        val usernameTextView: TextView = findViewById(R.id.usernameTextView)
-        usernameTextView.text = "SamFusedBits" // replace with actual user's name
+
+        val proceedButton = findViewById<Button>(R.id.proceed)
+
+        proceedButton.setOnClickListener {
+            val intent = Intent(this, PredictionActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
