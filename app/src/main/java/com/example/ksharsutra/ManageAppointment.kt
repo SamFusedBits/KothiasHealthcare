331583b34bf51e2008e575468f47f49e89b8dff6
Sahil Memon <sahil.infosphere@gmail.com>
2024-06-15 20:15:29 +0530

    Added data class ManageAppointment to represent appointment details.

---
+++ app/src/main/java/com/example/ksharsutra/ManageAppointment.kt
@@ -0,0 +1,10 @@
+package com.example.ksharsutra
+
+data class ManageAppointment(
+    var id: String = "",
+    var name: String = "",
+    var email: String = "",
+    var phone: String = "",
+    var schedule: String = "",
+    var status: String = ""
+)
