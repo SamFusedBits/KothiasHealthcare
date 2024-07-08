ee0b9a56655bb0f556a0e8400154e7f0140b2327
Sahil Memon <sahil.infosphere@gmail.com>
2024-07-08 21:00:01 +0530

    Add Questionnaire.kt

---
+++ app/src/main/java/com/example/ksharsutra/Questionnaire.kt
@@ -0,0 +1,11 @@
+package com.example.ksharsutra
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Questionnaire : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questionnaire)
+    }
+}
