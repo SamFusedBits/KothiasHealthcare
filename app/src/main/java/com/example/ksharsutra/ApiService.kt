bb6e263f071a2d5ae3f2482b542a5078c9455953
Sahil Memon <sahil.infosphere@gmail.com>
2024-07-26 11:19:10 +0530

    Added getPrediction endpoints.

--- app/src/main/java/com/example/ksharsutra/ApiService.kt
+++ app/src/main/java/com/example/ksharsutra/ApiService.kt
@@ -2,10 +2,14 @@ package com.example.ksharsutra
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ApiService {
 
     @POST("api/submit/")
     fun submitQuestionnaire(@Body questionnaire: Questionnaire): Call<ResponseData>
+
+    @GET("api/predict/")
+    fun getPrediction(): Call<PredictionResponse>
 }
