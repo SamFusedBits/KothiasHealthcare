7d6a97c1fed6f3e8c2cf67ba69731f03664d1bb7
Sahil Memon <sahil.infosphere@gmail.com>
2024-08-09 23:14:26 +0530

    Update dependencies in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,35 +67,45 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
+
+    // Firebase for Google Analytics, Performance Monitoring, Crashlytics, Realtime Database Firebase Messaging and In-App Messaging
     implementation("com.google.firebase:firebase-database:21.0.0")
     implementation("com.google.firebase:firebase-crashlytics:19.0.3")
     implementation("com.google.firebase:firebase-perf:21.0.1")
     implementation("com.google.firebase:firebase-analytics:22.0.2")
     implementation("com.google.firebase:firebase-messaging:24.0.0")
     implementation("com.google.firebase:firebase-inappmessaging-display:21.0.0")
-    testImplementation("junit:junit:4.13.2")
+
+    // Android JUnit Runner
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    testImplementation("junit:junit:4.13.2")
+
+    // Espresso core
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
+    // Google Play Services
+    implementation("com.google.android.gms:play-services-safetynet:17.0.0")
 
-
+    // Material Components
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     implementation("com.intuit.sdp:sdp-android:1.1.1")
     implementation("com.intuit.ssp:ssp-android:1.1.1")
 
+    // Firebase Authentication
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("com.google.firebase:firebase-firestore:25.0.0")
+
     // Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.android.gms:play-services-auth:21.2.0")
     implementation("com.google.firebase:firebase-firestore-ktx:24.0.0")
+
     // Firebase Storage
     implementation("com.google.firebase:firebase-storage-ktx")
     implementation("com.google.firebase:firebase-messaging")
@@ -104,10 +114,11 @@ dependencies {
     implementation("com.firebaseui:firebase-ui-firestore:7.2.0")
     implementation("com.google.firebase:firebase-storage:20.0.0")
 
+    // Firebase App Check
     implementation("com.google.firebase:firebase-appcheck:17.0.0")
 
+    // Retrofit and OkHttp
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
-
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
